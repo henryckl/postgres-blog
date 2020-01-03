85d863dedf2f928a77a78cee9f76a07b6a7365d1
Henryck <henryck.leandro@gmail.com>
2020-01-03 10:57:26 -0300

    SessionController adicionado

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import UserController from "./app/controllers/UserController";
 import PostController from "./app/controllers/PostController";
+import SessionController from "./app/controllers/SessionController";
 const routes = new Router();
 
 routes.post("/users", UserController.store);
 routes.post("/posts", PostController.store);
+routes.post("/sessions", SessionController.store);
 routes.get("/posts", PostController.index);
 
 export default routes;
